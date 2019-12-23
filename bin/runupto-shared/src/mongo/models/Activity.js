1e71f293b9aa0cd4ced2455c9ad8cdd99d3a00be
Jack Allen <jackallensemail+git@gmail.com>
2019-12-23 13:36:31 +0000

    change

--- bin/runupto-shared/src/mongo/models/Activity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var ActivitySchema = new mongoose_1.Schema({
-    description: String
-});
-exports.Activity = mongoose_1.model('Activity', ActivitySchema, 'Activities');
